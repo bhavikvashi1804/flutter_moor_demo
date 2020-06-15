4baff3175fa6adaa7d5a029236fecf4473b93fd2
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-06-15 11:40:39 +0530

    Generate file

---
+++ lib/data/moor_database.dart
@@ -0,0 +1,28 @@
+import 'package:moor/moor.dart';
+import 'package:moor_flutter/moor_flutter.dart';
+
+// Moor works by source gen. This file will all the generated code.
+part 'moor_database.g.dart';
+
+class Tasks extends Table {
+
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(min: 1, max: 50)();
+  DateTimeColumn get dueDate => dateTime().nullable()();
+  BoolColumn get completed => boolean().withDefault(Constant(false))();
+}
+
+@UseMoor(tables: [Tasks])
+
+class AppDatabase extends _$AppDatabase {
+  AppDatabase()
+      : super((FlutterQueryExecutor.inDatabaseFolder(
+          path: 'db.sqlite',
+          logStatements: true,
+        )));
+
+  @override
+  int get schemaVersion => 1;
+
+  
+}
