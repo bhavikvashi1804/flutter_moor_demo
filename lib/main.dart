ed5b12c79d4787d634d1cc90e7c9bd44bdffe2d5
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-06-15 12:12:25 +0530

    UI work

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_moor_tutorial/data/moor_database.dart';
+import 'package:provider/provider.dart';
+
+import './page/home_page.dart';
+
+
 
 void main() {
   runApp(MyApp());
@@ -8,31 +14,12 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Moor',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
- 
-  @override
-  Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Moor'),
+    return Provider(
+      builder: (context) => AppDatabase(),
+      child: MaterialApp(
+        title: 'Flutter Moor',
+        home: HomePage(),
       ),
-      body: Center(
-        child: Text('Flutter Moor Tutorial'),
-      )
     );
   }
 }
